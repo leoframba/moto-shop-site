f1c34533162ba2d5628985f470ea5b38b14b9d81
leoframba <leoframba@gmail.com>
2026-05-15 16:01:36 -0700

    frontend: enable proxy next config to deal with CORS errors

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	reactCompiler: true,
+
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination:
+					"https://moto-shop-api-276197149194.us-west1.run.app/api/:path*",
+			},
+		];
+	},
 };
 
 export default nextConfig;
